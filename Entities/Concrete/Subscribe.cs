0e0d45633cb909f7cca268e5284303532b7d8110
Oğuz Gül <oguz_gul216@hotmail.com>
2023-08-15 12:46:43 +0300

    Entity classes created.

---
+++ Entities/Concrete/Subscribe.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Subscribe:IEntity
+    {
+        public int SubscribeID { get; set; }
+        public string Mail { get; set; }
+    }
+}
---
+++ Entities/Concrete/Testimonial.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Testimonial:IEntity
+    {
+        public int TestimonialID { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
