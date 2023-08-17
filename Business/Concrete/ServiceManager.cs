97eb75c16dcd72cd1233a78915afbbc55ec46338
Oğuz Gül <oguz_gul216@hotmail.com>
2023-08-17 11:29:17 +0300

    Business service added.

---
+++ Business/Concrete/ServiceManager.cs
@@ -0,0 +1,62 @@
+﻿using Business.Abstract;
+using Business.Constant;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ServiceManager : IServiceService
+    {
+        IServiceDal _serviceDal;
+
+        public ServiceManager(IServiceDal serviceDal)
+        {
+            _serviceDal = serviceDal;
+        }
+
+        public IResult Add(Service service)
+        {
+            _serviceDal.Add(service);
+            return new SuccessResult(Messages.RoomAdded);
+        }
+
+        public IResult Delete(int id)
+        {
+            var deletedService = _serviceDal.Get(x=>x.ServiceID== id);
+            return new SuccessResult($"Deleted Data : {id} number's {Messages.ServiceDeleted}");
+        }
+
+        public IDataResult<List<Service>> GetAll()
+        {
+            var listedService = _serviceDal.GetAll();
+            return new SuccessDataResult<List<Service>>(listedService, Messages.ServiceGetAll);
+        }
+
+        public IDataResult<Service> GetById(int id)
+        {
+            var GetByIdService = _serviceDal.Get(x => x.ServiceID == id);
+            return new SuccessDataResult<Service>(GetByIdService, Messages.ServiceGetById);
+        }
+
+        public IResult Update(Service service)
+        {
+            var updateService = _serviceDal.Get(x => x.ServiceID == service.ServiceID);
+            Service updatedService = new Service
+            {
+                ServiceID = service.ServiceID,
+                Description = service.Description,
+                ServiceIcon = service.ServiceIcon,
+                Title = service.Title
+            };
+            _serviceDal.Update(updatedService);
+            return new SuccessResult(Messages.ServiceUpdated);
+            
+        }
+    }
+}
--- Business/Constant/Messages.cs
+++ Business/Constant/Messages.cs
@@ -13,5 +13,29 @@ namespace Business.Constant
         public static string RoomUpdated = "Room Updated";
         public static string RoomGetAll = "Get All Room";
         public static string RoomGetById = "Get Room";
+
+        public static string ServiceAdded = "Service Added";
+        public static string ServiceDeleted = "Service Deleted";
+        public static string ServiceUpdated = "Service Updated";
+        public static string ServiceGetAll = "Get All Service";
+        public static string ServiceGetById = "Get Service";
+
+        public static string StaffAdded = "Staff Added";
+        public static string StaffDeleted = "Staff Deleted";
+        public static string StaffUpdated = "Staff Updated";
+        public static string StaffGetAll = "Get All Staff";
+        public static string StaffGetById = "Get Staff";
+
+        public static string SubscribeAdded = "Subscribe Added";
+        public static string SubscribeDeleted = "Subscribe Deleted";
+        public static string SubscribeUpdated = "Subscribe Updated";
+        public static string SubscribeGetAll = "Get All Subscribe";
+        public static string SubscribeGetById = "Get Subscribe";
+
+        public static string TestimonialAdded = "Testimonial Added";
+        public static string TestimonialDeleted = "Testimonial Deleted";
+        public static string TestimonialUpdated = "Testimonial Updated";
+        public static string TestimonialGetAll = "Get All Testimonial";
+        public static string TestimonialGetById = "Get Testimonial";
     }
 }
